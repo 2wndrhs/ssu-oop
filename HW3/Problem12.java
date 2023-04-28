3cde2cfa3da664c65d6c398b827d1a6c099e3d00
2wndrhs <2wndrhs@gmail.com>
2023-04-29 02:02:39 +0900

    feat(HW3-Problem12): 시작 메시지를 출력하는 기능 구현

---
+++ HW3/Problem12.java
@@ -0,0 +1,16 @@
+package HW3;
+
+import java.util.Scanner;
+
+//  시작 메시지를 출력하는 기능
+// TODO 예약 시스템의 메뉴를 입력받는 기능
+public class Problem12 {
+
+  private static final String START_MESSAGE = "명품콘서트홀 예약 시스템입니다.";
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println(START_MESSAGE);
+  }
+}
