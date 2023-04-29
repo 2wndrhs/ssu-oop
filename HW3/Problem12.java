cadb5c04c15639b277b573a443dd5368c653c218
2wndrhs <2wndrhs@gmail.com>
2023-04-29 22:14:40 +0900

    feat(HW3-Problem12): "취소" 메뉴 입력 시 좌석의 등급을 입력받는 기능 구현

--- HW3/Problem12.java
+++ HW3/Problem12.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 // 예약자 이름과 좌석 번호를 입력받는 기능
 // 좌석 타입, 예약자 이름, 좌석 번호로 좌석을 예약하는 기능
 // "조회" 메뉴 입력 시 모든 좌석의 상태를 출력하는 기능
+// "취소" 메뉴 입력 시 좌석의 등급을 입력받는 기능
 class ReservationSystem {
 
   public static final int RESERVATION = 1;
@@ -106,6 +107,7 @@ public class Problem12 {
   private static final String SEAT_CLASS_INPUT_MESSAGE = "좌석구분 S(1), A(2), B(3)>>";
   private static final String NAME_INPUT_MESSAGE = "이름>>";
   private static final String SEAT_NUMBER_INPUT_MESSAGE = "번호>>";
+  private static final String INQUIRY_COMPLETE_MESSAGE = "<<<조회를 완료하였습니다.>>>";
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
@@ -132,8 +134,16 @@ public class Problem12 {
 
           reservationSystem.reserve(seatClass, name, seatNumber);
           break;
+
         case ReservationSystem.INQUIRY:
           reservationSystem.printAllLineState();
+          System.out.println(INQUIRY_COMPLETE_MESSAGE);
+          break;
+
+        case ReservationSystem.CANCEL:
+          System.out.print(SEAT_CLASS_INPUT_MESSAGE);
+          int seatClassCancel = scanner.nextInt();
+
           break;
       }
     }
