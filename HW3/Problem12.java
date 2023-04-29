c8a3371e6c548f07b792d2e4fa99adf840000fd5
2wndrhs <2wndrhs@gmail.com>
2023-04-29 21:14:26 +0900

    feat(HW3-Problem12): 예약자 이름과 좌석 번호를 입력받는 기능 구현

--- HW3/Problem12.java
+++ HW3/Problem12.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 //  예약 시스템의 메뉴를 입력받는 기능
 // "예약" 메뉴 입력 시 좌석의 등급을 입력받는 기능
 // 입력받은 좌석 등급의 현재 상태를 출력하는 기능
-
+// 예약자 이름과 좌석 번호를 입력받는 기능
 class ReservationSystem {
 
   public static final int RESERVATION = 1;
@@ -72,6 +72,8 @@ public class Problem12 {
   private static final String MENU_INPUT_MESSAGE = "예약:1, 조회:2, 취소:3, 끝내기:4>>";
   private static final String SEAT_CLASS_INPUT_MESSAGE = "좌석구분 S(1), A(2), B(3)>>";
   private static final String SEAT_STATE_MESSAGE = "%s>>";
+  private static final String NAME_INPUT_MESSAGE = "이름>>";
+  private static final String SEAT_NUMBER_INPUT_MESSAGE = "번호>>";
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
@@ -92,6 +94,13 @@ public class Problem12 {
       for (Seat seat : line.getSeats()) {
         System.out.print(" " + seat.getState());
       }
+      System.out.println();
+
+      System.out.print(NAME_INPUT_MESSAGE);
+      String name = scanner.next();
+
+      System.out.print(SEAT_NUMBER_INPUT_MESSAGE);
+      int seatNumber = scanner.nextInt();
     }
   }
 }
